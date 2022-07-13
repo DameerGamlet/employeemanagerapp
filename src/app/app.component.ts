7d5f0efaf214b222c16996a5e1b546def31643f7
DameerGamlet <gubaidulin.dam@gmail.com>
2022-07-13 17:50:00 +0400

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   employees: Employee[];
+  editEmployee: Employee;
+  deleteEmployee: Employee;
 
-  constructor(private employeeService: EmployeeService){}
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit() {
     this.getEmployees();
   }
 
-  public getEmployees(): void{
+  public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
         this.employees = response;
@@ -26,5 +29,94 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
     );
+  }
+
+  public onAddEmloyee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onUpdateEmloyee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteEmployee(employeeId: number): void {
+    document.getElementById('delete-employee-form')?.click();
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searcheEmployee(key: string): void {
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (
+        employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.numberPhone.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0) {
+      this.employees = [];
+    }
+    if (key.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
+  public onOpenModalGet(): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#addEmployeeModal');
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#editEmployeeModal');
+    }
+    if (mode === 'delete') {
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 }
--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,9 +1,10 @@
-export interface Employee{
-    id: number;
-    name: string;
-    email: string;
-    jobTitle: string;
-    numberPhone: string;
-    imageUrl: string;
-    employeeCode: string;
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  numberPhone: string;
+  imageUrl: string;
+  employeeCode: string;
+  dateCreated: string;
 }
